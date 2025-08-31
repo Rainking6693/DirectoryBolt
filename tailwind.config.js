023202d03dfc422e7d6dc31fe51d4d5da8a65eb2
DirectoryBolt Team <devops@directorybolt.com>
2025-08-31 09:43:52 -0600

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,29 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        volt: {
+          50:  '#f5fff0',
+          100: '#e6ffcc',
+          200: '#ccff99',
+          300: '#b3ff66',
+          400: '#99ff33',
+          500: '#80ff00',
+          600: '#66cc00',
+          700: '#4d9900',
+          800: '#336600',
+          900: '#193300',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
